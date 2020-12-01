a5ea9cef04cd134fcd607760fae5ee806b0b2e17
Matt <gamma032@gmail.com>
2020-12-01 16:13:50 +1100

    mend

---
+++ 0-template.py
@@ -0,0 +1,9 @@
+import math 
+import functools
+import re
+import random
+
+if __name__ == "__main__":
+    with open('./input/1-input.txt') as f:
+        data = f.read().splitlines()
+        print(data)
